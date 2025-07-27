2d9edc796c8daee109721ba9a2814307187f49bf
Smart-LU <139575137+LutzDi@users.noreply.github.com>
2025-07-27 19:09:11 +0200

    Update constants.py

--- strategies/constants.py
+++ strategies/constants.py
@@ -10,13 +10,13 @@ TRY_COUNT = 3
 DAILY_NOTIFICATION = True # Write True to send a daily notification, False to only send notifications when the signal changes
 
 # main subjects
-MAIN_SIGNAL_CHANGE_LONG = f"Halten (Cooldown ist aktiv seit {0} Tagen)"
-MAIN_SIGNAL_CHANGE_SHORT = f"In Cash wechseln (Cooldown ist aktiv seit {0} Tagen)"
+MAIN_SIGNAL_CHANGE_LONG = f"GO LONG NOW (cooldown activated for {0} days)"
+MAIN_SIGNAL_CHANGE_SHORT = f"GO IN CASH NOW (cooldown activated for {0} days)"
 COOLDOWN_WARNINGS = [1]
-COOLDOWN_WARNINGS_TEXT = ["Cooldown Warnung: Letzter Tag der Cooldown-Phase!"]
+COOLDOWN_WARNINGS_TEXT = ["Cooldown warning: Last day of cooldown"]
 
 # sub subjects
-INDICATOR_CHANGE_TITLE = "Einige Indikatoren haben sich geändert"
+INDICATOR_CHANGE_TITLE = "Some Indicators changed"
 assert len(COOLDOWN_WARNINGS) == len(COOLDOWN_WARNINGS_TEXT), "COOLDOWN_WARNINGS and COOLDOWN_WARNINGS_TEXT must have the same length"
 
 # ********************************** END SPY TIPS COOL STRATEGY **********************************
